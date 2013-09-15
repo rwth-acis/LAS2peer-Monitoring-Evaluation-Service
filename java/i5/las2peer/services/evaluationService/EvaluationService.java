c3e21d4fdbf60790f4072fee6ec97cc8d78912c8
Peter de Lange <peter.de.lange@rwth-aachen.de>
2013-09-15 21:13:50 +0200

    additional "infrastructure"

--- java/i5/las2peer/services/evaluationService/EvaluationService.java
+++ java/i5/las2peer/services/evaluationService/EvaluationService.java
@@ -13,11 +13,29 @@ import i5.las2peer.api.Service;
  */
 public class EvaluationService extends Service {
 	
+	
 	/**
+	 * 
 	 * Constructor: Loads the property file and enables the service monitoring.
+	 * 
 	 */
 	public EvaluationService(){
 		setFieldValues(); //This sets the values of the property file
 	}
+	
+	
+	/**
+	 * 
+	 * This method builds the entry point to this service. All parameters will be
+	 * set by the service's config file.
+	 * 
+	 */
+	public void sendMessages(){
+		System.out.println("Begin to send messages..");
+		while(true){
+			System.out.println("Next round..");
+		}
+	}
+	
 	
 }
